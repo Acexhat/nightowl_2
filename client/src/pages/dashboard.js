7d6731f24cf61f8126d55fb8bcec7c8f5c499030
akshatBhaskar <akshat.bhaskar@theflyy.com>
2022-03-28 01:56:01 +0530

    shiprocket api token done

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,10 +1,40 @@
 import * as React from 'react';
-
+import axios from 'axios'
 export default function Dashboard(props) {
+
+    const [userData, setUserData] = React.useState();
+
+    React.useEffect(() => {
+        // Get authenticated with shiprocket
+        var data = JSON.stringify({
+            "email": "ashish.kataria+hackathon@shiprocket.com",
+            "password": "hackathon@2022"
+        });
+
+        var config = {
+            method: 'post',
+            url: 'https://apiv2.shiprocket.in/v1/external/auth/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setUserData(response.data);
+                localStorage.setItem('ship_token', response.data.token);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }, [])
 
     return (
         <div>
-            TABLE
+
         </div>
     );
 }
