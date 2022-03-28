3950bdc95207b79dde0d6e02933aeb90da6f4568
akshatBhaskar <akshat.bhaskar@theflyy.com>
2022-03-28 08:50:21 +0530

    fixed deployment bug of moduke not fou

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 const ErrorResponse = require("../utils/errorResponse");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // @desc    Login user
 exports.login = async (req, res, next) => {
